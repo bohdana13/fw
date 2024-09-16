d4146249466cc9d2e45263433e063797dbfa4c08
spring_day <bogdanaredka2005@gmail.com>
2024-09-16 10:52:53 +0300

    init again

--- 3 lab/src/modal.ts
+++
@@ -1,9 +0,0 @@
-export class Modal {
-    open(message: string): void {
-        console.log('Modal opened with message:', message);
-    }
-
-    close(): void {
-        console.log('Modal closed');
-    }
-}

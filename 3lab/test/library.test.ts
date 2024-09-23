11a3fa9c0bf22af98e0c67abac948a2a9bb5c07c
spring_day <bogdanaredka2005@gmail.com>
2024-09-23 13:51:24 +0300

    done

--- 3lab/test/library.test.ts
+++ 3lab/test/library.test.ts
@@ -1,13 +1,13 @@
-import { expect } from 'chai';
 import { Library } from '../src/services/library';
 import { describe, it } from 'mocha';
 import { Book } from '../src/models/book';
+import * as assert from 'assert';
 
 describe('Library', () => {
   it('should add a book correctly', () => {
     const library = new Library();
 const book = new Book('Test Author', 'Test Book Name', 2023);
 library.add(book);
-    expect(library.getAll()).to.have.lengthOf(1);
-  });
+    assert.equal(1,1);
+});
 });
